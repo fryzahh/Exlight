2231222274ab6b6d10d30939455dab42702c2fe5
fryzahh <91803859+fryzahh@users.noreply.github.com>
2022-09-01 01:24:38 -0400

    Added some new Particle Stuff

---
+++ Particles/BasicSmokeParticle.cs
@@ -0,0 +1,56 @@
+﻿using Terraria;
+using Microsoft.Xna.Framework;
+using Exlight.Common.Systems.ParticleSystem;
+
+namespace Exlight.Particles
+{
+    public class BasicSmokeParticle : Particle
+    {
+        public override string Texture => "Exlight/Particles/Smoke";
+        public override int ParticleFrameCount => 3;
+
+        private float Opacity;
+        private float RotationSpeed;
+        private Color StartColor;
+        private Color FadeOutColor;
+        private bool AffectedByWindSpeed;
+
+        public BasicSmokeParticle(Vector2 position, Vector2 velocity, Color startColor, Color fadeColor, float scale, float opacity, float rotationSpeed, bool affectedByWindSpeed = false)
+        {
+            Position = position;
+            Velocity = velocity;
+            StartColor = startColor;
+            FadeOutColor = fadeColor;
+            Frame = Main.rand.Next(3);
+            Scale = scale;
+            Opacity = opacity;
+            RotationSpeed = rotationSpeed;
+            AffectedByWindSpeed = affectedByWindSpeed;
+        }
+
+        public override void Update()
+        {
+            Color = Color.Lerp(StartColor, FadeOutColor, MathHelper.Clamp((255f - Opacity - 100f) / 70f, 0f, 1f)) * (Opacity / 255f);
+            Rotation += RotationSpeed * ((Velocity.X > 0) ? 1f : -1f);
+            Velocity *= 0.96f;
+            if (AffectedByWindSpeed)
+            {
+                Velocity.X += Main.windSpeedCurrent * 0.06f;
+            }
+
+            if (Opacity > 80f)
+            {
+                Scale += 0.01f;
+                Opacity -= 3f;
+            }
+            else
+            {
+                Scale *= 0.985f;
+                Opacity -= 2f;
+            }
+
+            if (Opacity < 0f)
+                Kill();
+        }
+    }
+}
---
+++ Particles/ScaleWithTimeParticle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Exlight.Common.Systems.ParticleSystem;
+using Terraria.ModLoader;
+using ReLogic.Content;
+
+namespace Exlight.Particles
+{
+    public class ScaleWithTimeParticle : Particle
+    {
+        public override string Texture => "Exlight/Textures/BloomLight";
+
+        public override bool UseAdditiveBlending => true;
+        public override bool UseCustomDrawing => true;
+        public override bool HasSetLifetime => true;
+
+        private float Opacity;
+        private Color BaseColor;
+        private bool AffectedByWindSpeed;
+
+        public ScaleWithTimeParticle(Vector2 position, Vector2 velocity, Color color, float scale, int maxTime, bool affectedByWindSpeed = false)
+        {
+            Position = position;
+            Velocity = velocity;
+            BaseColor = color;
+            Scale = scale;
+            MaxTime = maxTime;
+            AffectedByWindSpeed = affectedByWindSpeed;
+        }
+
+        public override void Update()
+        {
+            Lighting.AddLight(Position, Color.ToVector3() * Scale);
+            Opacity = (float)Math.Sin((double)((MaxTime - LifeTime) / 255f) * (float)Math.PI);
+            Color = BaseColor * Opacity;
+
+            Scale = Utils.GetLerpValue(MaxTime, LifeTime / 3, LifeTime, true);
+
+            Velocity *= 0.97f;
+            if (AffectedByWindSpeed)
+                Velocity.X = Velocity.X + Main.windSpeedCurrent * 0.07f;
+        }
+
+        public override void CustomDrawing(SpriteBatch spriteBatch)
+        {
+            Texture2D texture = ModContent.Request<Texture2D>(Texture, (AssetRequestMode)2).Value;
+            Texture2D bloom = ModContent.Request<Texture2D>("Exlight/Textures/BloomCircle", (AssetRequestMode)2).Value;
+            spriteBatch.Draw(bloom, Position - Main.screenPosition, null, Color, Rotation, bloom.Size() / 2, Scale / 5f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(texture, Position - Main.screenPosition, null, Color, Rotation, texture.Size() / 2f, Scale, SpriteEffects.None, 0f);
+        }
+    }
+}
